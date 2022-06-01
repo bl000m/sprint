8bcff7dff7a0db63e03900141e00ee9acff8475b
mathia pagani <mathiapagani@gmail.com>
2022-06-01 17:59:50 +0200

    integration ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_31_143826) do
+ActiveRecord::Schema.define(version: 2022_06_01_154504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,8 @@ ActiveRecord::Schema.define(version: 2022_05_31_143826) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.integer "trello_id"
+    t.string "token"
+    t.string "secret"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
