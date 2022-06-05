2c6d7ac802d4fbb6228cb8a3404bf5ad86a57aa1
mathia pagani <mathiapagani@gmail.com>
2022-06-05 15:25:22 +0200

    bug fixed : last version Mathia + last version Cedric restored. last version Stef not included

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,25 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks
   end
 
   def show
     @task = Task.find(params[:id])
     @review = Review.new
+  end
+
+  #ici create cards
+  # def create
+  #   @task = Task.new(task_params)
+  #   if @task.save
+  #     redirect_to new_projects_url
+  #   else
+  #     render "projects/new"
+  #   end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :desc, :trello_id_member )
   end
 end
