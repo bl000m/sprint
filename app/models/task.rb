e5ce5c42252434a31c767865d984e8ca9163f321
mathia pagani <mathiapagani@gmail.com>
2022-06-06 18:32:13 +0200

    format time passing fixed

--- app/models/task.rb
+++ app/models/task.rb
@@ -9,6 +9,14 @@ class Task < ApplicationRecord
       *,
       extract(epoch from (end_at - created_at)) AS difference
     SQL
-    missions.where.not(end_at: nil).select(select_sql).map(&:difference).sum
+    if missions.where(end_at: nil)
+      seconds_to_hms(missions.where.not(end_at: nil).select(select_sql).map(&:difference).sum)
+    else
+      seconds_to_hms(missions.where.not(end_at: nil).select(select_sql).map(&:difference).sum)
+    end
+  end
+
+  def seconds_to_hms(sec)
+    "%02d:%02d:%02d" % [sec / 3600, sec / 60 % 60, sec % 60]
   end
 end
