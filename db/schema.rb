65dec84e11f928442ba235fcd9f2af2ecbdbe280
mathia pagani <mathiapagani@gmail.com>
2022-06-08 19:06:39 +0200

    add migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_08_085442) do
+ActiveRecord::Schema.define(version: 2022_06_08_170230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2022_06_08_085442) do
     t.string "trello_board_id"
     t.string "trello_list_id"
     t.string "trello_done_list_id"
+    t.integer "trello_field_estimated_time_id"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
